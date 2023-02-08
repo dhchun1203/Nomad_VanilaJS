39a053cd18d86515e4e2f328fae36a32339d9db2
dhchun <dhchun1203@gmail.com>
2023-02-08 11:06:27 +0900

    Deleting Todos

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,19 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.querySelector("#todo-list");
 
+function deleteTodo(e) {
+  const li = e.target.parentElement;
+  li.remove();
+}
+
 function paintTodo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteTodo);
   li.appendChild(span);
+  li.appendChild(button);
   span.innerText = newTodo;
   todoList.appendChild(li);
 }
