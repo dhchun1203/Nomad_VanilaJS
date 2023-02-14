c71be0b0c50f4948c88e85441d33fbefdae5e6fd
dhchun <dhchun1203@gmail.com>
2023-02-14 22:55:46 +0900

    Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,17 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY = "80700df28356ed972e31283dbbd99ed0";
+
 function onGeoOK(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log("You live in", lat, lng);
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      city.innerHTML = data.name;
+      weather.innerHTML = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 
 function onGeoError() {
